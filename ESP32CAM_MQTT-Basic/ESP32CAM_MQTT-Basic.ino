92b6ad122d30512900fa8601f98b165b5d53c14b
carls70 <carloso70@yahoo.com.mx>
2022-03-10 08:00:53 -0600

    Update ESP32CAM_MQTT-Basic.ino

--- ESP32CAM_MQTT-Basic/ESP32CAM_MQTT-Basic.ino
+++ ESP32CAM_MQTT-Basic/ESP32CAM_MQTT-Basic.ino
@@ -18,12 +18,12 @@
 #include <PubSubClient.h> //Biblioteca para conexion MQTT
 
 //Datos de WiFi
-const char* ssid = "AXTEL XTREMO-18D6";  // Aquí debes poner el nombre de tu red
-const char* password = "038C18D6";  // Aquí debes poner la contraseña de tu red
+const char* ssid = "TP-LINK_349952";  // Aquí debes poner el nombre de tu red
+const char* password = "1F349952";  // Aquí debes poner la contraseña de tu red
 
 //Datos del broker MQTT
-const char* mqtt_server = "192.168.15.33"; // Si estas en una red local, coloca la IP asignada, en caso contrario, coloca la IP publica
-IPAddress server(192,168,15,33);
+const char* mqtt_server = "172.16.34.123"; // Si estas en una red local, coloca la IP asignada, en caso contrario, coloca la IP publica
+IPAddress server(172,16,34,123);
 
 // Objetos
 WiFiClient espClient; // Este objeto maneja los datos de conexion WiFi
